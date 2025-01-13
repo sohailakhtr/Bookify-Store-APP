946805e07db801df50ce91c4a54f7b5421998553
sohailakhtr <soh7132s@gmail.com>
2025-01-13 15:56:11 +0500

    Frontend done

---
+++ frontend/src/components/Freebook.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+import Cards from "./Cards"; // Correct import of Cards component
+
+import list from "../../public/list.json"; // Assuming list.json is in the public folder
+
+function Freebook() {
+  // Filter data based on category "Free"
+  const filterData = list.filter((data) => data.category === "Free");
+
+  // Slider settings
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+      <div>
+        <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis,
+          nesciunt, harum eos sequi quos accusamus sint. Tempore excepturi ipsam
+          adipisci harum eos sequi.
+        </p>
+      </div>
+
+      <div>
+        <Slider {...settings}>
+          {filterData.map((item) => (
+            <Cards item={item} key={item.id} />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+}
+
+export default Freebook;
